53c3120ddec509a19994ccfe2934b308f78dd28c
Johannes Diehl <johannes@diehl.io>
2022-05-15 23:05:17 +0200

    fix missing status subscription on config change, fix disconnecting shared socket when deleting one of multiple instances

--- src/UdpSocket.ts
+++ src/UdpSocket.ts
@@ -1,87 +1,87 @@
-import { EventEmitter } from 'events'
-import { Socket } from 'dgram'
-import * as dgram from 'dgram'
+import { EventEmitter } from "events";
+import { Socket } from "dgram";
+import * as dgram from "dgram";
 
 export class UdpSocket extends EventEmitter {
-	public static getInstance() {
-		if (!this._instance) {
-			this._instance = new UdpSocket()
-		}
-		return this._instance
-	}
+  public static getInstance() {
+    if (!this._instance) {
+      this._instance = new UdpSocket();
+    }
+    return this._instance;
+  }
 
-	private static _instance
+  private static _instance;
 
-	private _started = false
+  private _started = false;
 
-	private _socket: Socket
+  private _socket: Socket;
 
-	private _state: SocketState = SocketState.CLOSED
+  private _state: SocketState = SocketState.CLOSED;
 
-	constructor() {
-		super()
-	}
+  constructor() {
+    super();
+  }
 
-	public get state() {
-		return this._state
-	}
+  public get state() {
+    return this._state;
+  }
 
-	public start(): void {
-		if (this._started && (this._state === SocketState.OPENED || this._state === SocketState.STARTING)) {
-			return
-		}
-		this.setState(SocketState.STARTING)
-		this._started = true
+  public start(): void {
+    if (this._started && (this._state === SocketState.OPENED || this._state === SocketState.STARTING)) {
+      return;
+    }
+    this.setState(SocketState.STARTING);
+    this._started = true;
 
-		this._socket = dgram.createSocket({
-			type: 'udp4',
-		})
+    this._socket = dgram.createSocket({
+      type: "udp4",
+    });
 
-		this._socket.on('listening', () => {
-			this.setState(SocketState.OPENED)
-			const address = this._socket.address()
-			console.log(`[Sennheiser EW] socket listening ${address.address}:${address.port}`)
-		})
+    this._socket.on("listening", () => {
+      this.setState(SocketState.OPENED);
+      const address = this._socket.address();
+      console.log(`[Sennheiser EW] socket listening ${address.address}:${address.port}`);
+    });
 
-		this._socket.on('message', (msg, rinfo) => {
-			//console.log(`[Sennheiser EW] server got: ${msg.toString().replace("\r", "\\r").replace("\n", "\\n")} from ${rinfo.address}:${rinfo.port}`);
-			this.emit(`message:${rinfo.address}`, msg, rinfo)
-		})
+    this._socket.on("message", (msg, rinfo) => {
+      //console.log(`[Sennheiser EW] server got: ${msg.toString().replace("\r", "\\r").replace("\n", "\\n")} from ${rinfo.address}:${rinfo.port}`);
+      this.emit(`message:${rinfo.address}`, msg, rinfo);
+    });
 
-		this._socket.on('error', (err) => {
-			this.setState(SocketState.ERROR)
-			console.log(`[Sennheiser EW] server error:\n${err.stack}`)
-		})
+    this._socket.on("error", (err) => {
+      this.setState(SocketState.ERROR);
+      console.log(`[Sennheiser EW] server error:\n${err.stack}`);
+    });
 
-		this._socket.on('close', () => {
-			this.setState(SocketState.CLOSED)
-			console.log(`[Sennheiser EW] server closed`)
-		})
+    this._socket.on("close", () => {
+      this.setState(SocketState.CLOSED);
+      console.log(`[Sennheiser EW] server closed`);
+    });
 
-		this._socket.bind({
-			address: '0.0.0.0',
-			port: 53212,
-		})
-	}
+    this._socket.bind({
+      address: "0.0.0.0",
+      port: 53212,
+    });
+  }
 
-	private setState(state: SocketState) {
-		this._state = state
-		this.emit('state', state)
-	}
+  private setState(state: SocketState) {
+    this._state = state;
+    this.emit("state", state);
+  }
 
-	public close() {
-		this.setState(SocketState.CLOSED)
-		this._socket.close()
-	}
+  public close() {
+    this.setState(SocketState.CLOSED);
+    this._socket.close();
+  }
 
-	public send(message: string, target: string) {
-		this._socket.send(message, 53212, target)
-	}
+  public send(message: string, target: string) {
+    this._socket.send(message, 53212, target);
+  }
 }
 
 export enum SocketState {
-	CLOSED,
-	OPENED,
-	ERROR,
-	STARTING
+  CLOSED,
+  OPENED,
+  ERROR,
+  STARTING,
 }
